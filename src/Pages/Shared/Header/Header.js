f883713de122e7d6d58d42744e7cda63b3c2383b
tayefulislam <kazitayefulislam12@gmail.com>
2024-01-03 15:45:15 +0600

    header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div class="navbar bg-[FFFFFF]">
+        <div class="navbar-start">
+          <Link to={"/"} class="btn btn-ghost normal-case text-xl">
+            Priyo Pathshala
+          </Link>
+        </div>
+
+        <div class="navbar-end">
+          <Link className="text-xl font-semibold text-red-500 mx-1" to="/menu">
+            MENU
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
